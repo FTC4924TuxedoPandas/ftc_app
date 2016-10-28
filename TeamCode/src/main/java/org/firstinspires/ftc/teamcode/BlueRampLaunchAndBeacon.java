9493b463caa6c63101b8cd9d44ad9f387d0aae62
4924_Users <nrrobotics@gmail.com>
2016-10-28 16:29:14 -0400

    Created BlueRampLaunchAndBeacon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRampLaunchAndBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRampLaunchAndBeacon.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 /**
  * Created by 4924_Users on 10/22/2016.
  */
 
 @Autonomous(name = "RedRampLaunchAndBeacon")
-public class RedRampLaunchAndBeacon extends LaunchAndBeaconBase {
+public class BlueRampLaunchAndBeacon extends LaunchAndBeaconBase {
 
-    public RedRampLaunchAndBeacon() {
+    public BlueRampLaunchAndBeacon() {
 
         launchPositioningPath = new DrivePathSegment[]{
 
