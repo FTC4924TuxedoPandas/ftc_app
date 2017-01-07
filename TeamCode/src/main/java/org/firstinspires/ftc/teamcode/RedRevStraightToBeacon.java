a3a7260e4b94122b3670ca060bb8a2ae7c236da2
4924_Users <nrrobotics@gmail.com>
2017-01-07 14:28:52 -0500

    Added Revolution "Straight to Beacon" Program (Red)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRevStraightToBeacon.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by 4924_Users on 1/7/2017.
+ */
+
+@Autonomous(name = "RedRevStraightToBeacon")
+public class RedRevStraightToBeacon extends RevStraightToBeaconBase {
+
+    public RedRevStraightToBeacon() {
+
+        beaconPath = new DrivePathSegment[]{
+
+                new DrivePathSegment(4.0f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(315.0f, 0.3f, DrivePathSegment.TURN),
+                new DrivePathSegment(45.0f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(270.0f, 0.3f, DrivePathSegment.TURN),
+                new DrivePathSegment(2.0f, 0.1f, DrivePathSegment.LINEAR),
+        };
+    }
+
+    @Override
+    public boolean isRed() {
+
+        return true;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevStraightToBeaconBase.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by 4924_Users on 1/7/2017.
+ */
+
+public abstract class RevStraightToBeaconBase extends RevolutionAutonomousBase {
+
+    @Override
+    public State[] stateList() {
+
+        return new State[] {
+
+                State.STATE_INITIAL,
+                State.STATE_START_BEACON_PATH,
+                State.STATE_DRIVE,
+                State.STATE_FIND_WHITE_LINE,
+                State.STATE_PUSH_BEACON,
+                State.STATE_FIND_WHITE_LINE,
+                State.STATE_PUSH_BEACON,
+                State.STATE_STOP
+        };
+    }
+}
