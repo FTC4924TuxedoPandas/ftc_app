b4d85f693cda14f713ab063665a7979a9be891b8
FTC4924 <nrrobotics@gmail.com>
2017-01-21 18:59:21 -0500

    Added spinningServo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRevLaunchAndCapBallBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRevLaunchAndCapBallBeacon.java
@@ -12,7 +12,7 @@ public class BlueRevLaunchAndCapBallBeacon extends RevLaunchAndCapBallBeaconBase
         launchPositioningPath = new DrivePathSegment[]{
 
                 new DrivePathSegment(2.00f, 0.3f, DrivePathSegment.LINEAR),
-                new DrivePathSegment(36.0f, 0.3f, DrivePathSegment.TURN), //YEAH BOOOOOOOOOOOOOOOOOOOOOIIIIIIIIIIIIIIIIIIIIIIII
+                new DrivePathSegment(36.0f, 0.3f, DrivePathSegment.TURN),
                 new DrivePathSegment(15.0f, 0.3f, DrivePathSegment.LINEAR),
                 new DrivePathSegment(50.0f, 0.3f, DrivePathSegment.TURN),
                 new DrivePathSegment(1.0f),
