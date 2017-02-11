88d7fd9cc76a272922aab7951feab24448692e73
FTC4924 <nrrobotics@gmail.com>
2017-02-11 15:27:17 -0500

    Increased arm power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevolutionAutonomousBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevolutionAutonomousBase.java
@@ -796,7 +796,8 @@ public abstract class RevolutionAutonomousBase extends RevolutionVelocityBase {
 
         } else {
 
-            throwingArmPowerLevel = 0.8f;
+            throwingArmPowerLevel = 0.9f;
+            //throwingArmPowerLevel = 0.8f; //for when gearbox starts wearing down
         }
     }
 
