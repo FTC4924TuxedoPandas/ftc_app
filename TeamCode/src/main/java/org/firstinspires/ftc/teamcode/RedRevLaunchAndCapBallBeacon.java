2ea8f845e889ed7d8b093973f6aef499c7d037fd
FTC4924 <nrrobotics@gmail.com>
2017-02-18 16:42:27 -0500

    Added launch and ramp program for red - SW

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRevLaunchAndCapBallBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRevLaunchAndCapBallBeacon.java
@@ -23,8 +23,8 @@ public class RedRevLaunchAndCapBallBeacon extends RevLaunchAndCapBallBeaconBase 
                 new DrivePathSegment(315.0f, 0.3f, DrivePathSegment.TURN),
                 new DrivePathSegment(20.0f, 0.4f, DrivePathSegment.LINEAR),
                 new DrivePathSegment(1.0f),
-                new DrivePathSegment(288.0f, 0.3f, DrivePathSegment.TURN),
-                new DrivePathSegment(12.0f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(281.0f, 0.2f, DrivePathSegment.TURN),
+                new DrivePathSegment(12.0f, 0.2f, DrivePathSegment.LINEAR),
         };
         stop = new DrivePathSegment [] {
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRevLaunchAndRamp.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by 4924_Users on 2/18/2017.
+ */
+@Autonomous(name = "RedRevLaunchAndRamp", group = "Revolution")
+public class RedRevLaunchAndRamp extends RevLaunchAndRampBase{
+    public RedRevLaunchAndRamp(){
+        launchPositioningPath = new DrivePathSegment[]{
+
+                new DrivePathSegment(2.00f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(324.0f, 0.3f, DrivePathSegment.TURN),
+                new DrivePathSegment(15.0f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(310.0f, 0.3f, DrivePathSegment.TURN),
+                new DrivePathSegment(1.0f),
+        };
+
+        beaconPath = new DrivePathSegment[] {
+
+                new DrivePathSegment(-7.0f, 0.3f, DrivePathSegment.HOLONOMIC),
+                new DrivePathSegment(8.0f, 0.3f, DrivePathSegment.LINEAR),
+                new DrivePathSegment(220.0f, 0.3f, DrivePathSegment.TURN),
+                new DrivePathSegment(5.0f, 0.3f, DrivePathSegment.LINEAR),
+        };
+    }
+    @Override
+    public boolean isRed() {
+
+        return true;
+    }
+}
