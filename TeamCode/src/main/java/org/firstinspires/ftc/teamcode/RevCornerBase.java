8cf3109c9bbcb3fbbc91ee12323f49c9bffae70d
FTC4924 <nrrobotics@gmail.com>
2017-03-11 11:02:13 -0500

    Added RevCornerBase program incorporating steps for ball launching, knocking cap ball, and driving to the corner ramp.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevCornerBase.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by 4924_Users on 3/11/2017.
+ */
+
+public abstract class RevCornerBase extends RevolutionAutonomousBase {
+    @Override
+    public State[] stateList() {
+
+        return new State[] {
+
+                State.STATE_INITIAL,
+                State.STATE_START_LAUNCH_PATH,
+                State.STATE_POSITION_FOR_BALL,
+                State.STATE_LAUNCH_BALL,
+                State.STATE_LOAD_BALL,
+                State.STATE_WAIT_FOR_BALL,
+                State.STATE_LAUNCH_BALL,
+                State.STATE_START_CAP_BALL_PATH,
+                State.STATE_KNOCK_CAP_BALL,
+                State.STATE_START_RAMP_PATH,
+                State.STATE_DRIVE_TO_RAMP,
+                State.STATE_STOP
+        };
+    }
+}
